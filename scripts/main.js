049bb89a5b82286f4b5255e2b6df42712cdfe201
DRairigh16 <dietrichrairigh@gmail.com>
2025-09-01 11:49:20 -0400

    Update main.js
    Change what is cool.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,20 @@
-const myHeading = document.querySelector("h1");
+const myHeading = document.querySelector("h1")
 const myButton = document.querySelector("button")
 const myImage = document.querySelector("img")
+let cool = "Mozilla"
+
 
 myImage.addEventListener("click", ()=>{
     const src = myImage.getAttribute("src")
     if (src == "images/firefox-icon.png"){
         myImage.setAttribute("src", "images/firefox-icon2.png")
+        myHeading.textContent = `Sprint 127 is cool, ${localStorage.getItem("name")}`
+        cool = "Sprint 127" 
     }
     if (src == "images/firefox-icon2.png"){
         myImage.setAttribute("src", "images/firefox-icon.png")
+        myHeading.textContent = `Mozilla is cool, ${localStorage.getItem("name")}`
+        cool = "Mozilla"
     }
 })
 
@@ -19,7 +25,7 @@ function setUserName(){
     }
     else{
         localStorage.setItem("name", myName)
-        myHeading.textContent = `Mozilla is cool, ${myName}`}
+        myHeading.textContent = `${cool} is cool, ${myName}`}
 }
 if (!localStorage.getItem("name")){
     setUserName()
