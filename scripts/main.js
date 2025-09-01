9817016e941534a450a50d89162e051bd441438d
DRairigh16 <dietrichrairigh@gmail.com>
2025-09-01 14:12:40 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const myHeading = document.querySelector("h1")
 const myText = document.querySelector("p")
+const myLink = document.querySelector("a")
 const myButton = document.querySelector("button")
 const myImage = document.querySelector("img")
 let cool = "Mozilla"
@@ -12,12 +13,14 @@ myImage.addEventListener("click", ()=>{
         myHeading.textContent = `Sprint 127 is cool, ${localStorage.getItem("name")}`
         cool = "Sprint 127"
         myText.textContent = myText.textContent.replaceAll("Mozilla", "Sprint 127");
+        myLink.textContent = myLink.textContent.replaceAll("Mozilla", "Sprint 127");
     }
     if (src == "images/firefox-icon2.png"){
         myImage.setAttribute("src", "images/firefox-icon.png")
         myHeading.textContent = `Mozilla is cool, ${localStorage.getItem("name")}`
         cool = "Mozilla"
         myText.textContent = myText.textContent.replaceAll("Sprint 127", "Mozilla");
+        myLink.textContent = myLink.textContent.replaceAll("Sprint 127", "Mozilla");
     }
 })
 
