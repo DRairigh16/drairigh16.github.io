fec390be51920f91dfe5c408182caf2855bd0c1e
DRairigh16 <dietrichrairigh@gmail.com>
2025-09-23 16:22:33 -0400

    Create Start.js for project First Pro

---
+++ First Pro/src/scenes/Start.js
@@ -0,0 +1,55 @@
+import {Player} from "./../gameObjects/Player.js"
+export class Boom extends Phaser.Scene {
+
+
+    constructor() {
+        super('Boom');
+    }
+
+    preload ()
+    {
+        this.load.image('sky', 'assets/assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'sky');
+
+
+        this.platforms = this.physics.add.staticGroup();
+
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+
+        this.player = new Player(this, 100, 450);
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update ()
+    {
+        if (this.cursors.left.isDown){
+            this.player.moveLeft()
+        }
+        else if (this.cursors.right.isDown){
+            this.player.moveRight()
+        }
+        else {
+            this.player.idle()
+        }
+
+        if (this.cursors.up.isDown){
+            this.player.jump()
+        }
+       
+}
+}
