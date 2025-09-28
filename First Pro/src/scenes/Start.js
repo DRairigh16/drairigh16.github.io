f5f77e0eb7206da183937b7da77bd94961c0b7b5
DRairigh16 <dietrichrairigh@gmail.com>
2025-09-28 16:00:08 -0400

    Update Start.js

--- First Pro/src/scenes/Start.js
+++ First Pro/src/scenes/Start.js
@@ -9,7 +9,7 @@ export class Boom extends Phaser.Scene {
 
     preload ()
     {
-        this.load.image('sky', 'assets/assets/sky.png');
+        this.load.image('sky', 'assets/sky.png');
         this.load.image('ground', 'assets/platform.png');
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
