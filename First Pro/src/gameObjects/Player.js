374a4bda3c723b4538219aa98f2050012f3fc29f
DRairigh16 <dietrichrairigh@gmail.com>
2025-09-28 15:56:05 -0400

    Update Player.js

--- First Pro/src/gameObjects/Player.js
+++ First Pro/src/gameObjects/Player.js
@@ -7,6 +7,10 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
         this.setBounce(0.2);
         this.setCollideWorldBounds(true);
         this.initAnimations();
+        this.power_ups = {"bounce": false, "speed": 1, "shield": false, "magnet": false, "fly": 1}
+        this.pow_img = this.scene.add.group();
+        this.pow_mask = this.createBitmapMask();
+        this.pow_mask.invertAlpha = true;
     }
 
     initAnimations(){
@@ -33,12 +37,12 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
     }
 
     moveLeft(){
-        this.setVelocityX(-200);
+        this.setVelocityX(-200*this.power_ups.speed);
         this.anims.play("left", true);
     }
 
     moveRight(){
-        this.setVelocityX(200);
+        this.setVelocityX(200*this.power_ups.speed);
         this.anims.play("right", true);
     }
 
@@ -48,8 +52,35 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
     }
 
     jump(){
-        if (this.body.blocked.down){
-            this.setVelocityY(-500);
+        if (this.body.blocked.down || this.power_ups.fly > 1){
+            this.setVelocityY(-500/this.power_ups.fly);
         }
+    }
+    updatePow(){
+        this.pow_img.getChildren().forEach((sprite)=>{
+            if (sprite.mask != this.pow_mask){
+                sprite.setMask(this.pow_mask);
+            }
+            sprite.setX(this.x);
+            sprite.setY(this.y);
+        })
+    }
+    getPowerUp(powerup){
+        ["bounce", "speed", "shield", "magnet", "fly"].forEach((value)=>{
+            if (value==powerup){
+                this.pow_img.create(this.x, this.y, powerup);
+                var child = this.pow_img.children.getArray().slice(-1)[0];
+                if (powerup == "speed" || powerup == "fly"){
+                    this.power_ups[powerup] = 2;
+                    this.scene.time.delayedCall(20000, (child)=>{this.power_ups[powerup] = 1; child.destroy();}, [child]);
+                }
+                else if(powerup != "shield"){
+                    this.power_ups[powerup] = true;
+                    this.scene.time.delayedCall(20000, (child)=>{this.power_ups[powerup] = false; child.destroy();}, [child]);
+                }
+                else{
+                    this.power_ups[powerup] = true;
+                    this.shield = this.pow_img.children.getArray().slice(-1)[0];}
+        }})
     }
 }
